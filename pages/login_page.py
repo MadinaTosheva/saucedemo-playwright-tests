a00a4f2ceb21c33545f8f3fbb7419d8bdcccb1e9
madinabonu.tosheva <madinatoseva4@gmail.com>
2026-04-22 11:27:37 +0500

    Исправила комменты под test_Auth

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.username_input = self.page.locator("#user-name")
         self.password_input = self.page.locator("#password")
         self.login_button = self.page.locator("#login-button")
-        self.error = self.page.locator(".error-message-container")
+        self.error = self.page.locator(".error-message-container h3")
 
     def enter_username(self, username):
         self.username_input.fill(username)
@@ -32,4 +32,11 @@ class LoginPage(BasePage):
     def validate_error_text(self, error_text):
         expect(self.error).to_have_text(error_text)
 
+    def login_procedure(self, username_, password_):
+        self.enter_username(username_)
+        self.enter_password(password_)
+        self.click_login_btn()
 
+    def error_check(self, error_text):
+        self.validate_visibility_of_error()
+        self.validate_error_text(error_text)
--- tests/test_Auth.py
+++ tests/test_Auth.py
@@ -1,3 +1,5 @@
+import pytest
+
 from config.base import BASE_URL, BASE_ROOT, INVENTORY_ENDPOINT
 from config.users import STANDARD_USER, PASSWORD, LOCKED_OUT_USER, \
     LOCKED_OUT_USER_ERROR, WRONG_PASSWORD, NO_MATCHES_ERROR, FAKE_USER, \
@@ -7,6 +9,24 @@ from pages.base_page import BasePage
 from pages.inventory_page import InventoryPage
 from pages.login_page import LoginPage
 
+@pytest.mark.parametrize("username, password, error_text",[
+    (LOCKED_OUT_USER, PASSWORD, LOCKED_OUT_USER_ERROR),
+    (STANDARD_USER, WRONG_PASSWORD, NO_MATCHES_ERROR),
+    (FAKE_USER, PASSWORD, NO_MATCHES_ERROR),
+    (EMPTY_USER, PASSWORD, USERNAME_EMPTY_ERROR),
+    (STANDARD_USER, EMPTY_PASSWORD, PASSWORD_EMPTY_ERROR),
+    (WRONG_SQL_USER, PASSWORD, NO_MATCHES_ERROR),
+    (WRONG_XSS_USER, PASSWORD, NO_MATCHES_ERROR)])
+
+def test_negative_login(page, username, password, error_text):
+    # покрывает тест кейсы от 2 до 8
+    base_page = BasePage(page)
+    base_page.open_page(BASE_URL)
+    base_page.validate_url(BASE_URL)
+
+    login_page = LoginPage(page)
+    login_page.login_procedure(username, password)
+    login_page.error_check(error_text)
 
 def test_tc_auth_001(page):
     base_page = BasePage(page)
@@ -14,120 +34,27 @@ def test_tc_auth_001(page):
     base_page.validate_url(BASE_URL)
 
     login_page = LoginPage(page)
-    login_page.enter_username(STANDARD_USER)
-    login_page.validate_username(STANDARD_USER)
-    login_page.enter_password(PASSWORD)
-    login_page.validate_password(PASSWORD)
-    login_page.click_login_btn()
+    login_page.login_procedure(STANDARD_USER, PASSWORD)
 
     inventory_page = InventoryPage(page)
     inventory_page.validate_url(BASE_ROOT + INVENTORY_ENDPOINT)
 
-def test_tc_auth_002(page):
-    base_page = BasePage(page)
-    base_page.open_page(BASE_URL)
-    base_page.validate_url(BASE_URL)
-
-    login_page = LoginPage(page)
-    login_page.enter_username(LOCKED_OUT_USER)
-    login_page.validate_username(LOCKED_OUT_USER)
-    login_page.enter_password(PASSWORD)
-    login_page.validate_password(PASSWORD)
-    login_page.click_login_btn()
-    login_page.validate_visibility_of_error()
-    login_page.validate_error_text(LOCKED_OUT_USER_ERROR)
-
-def test_tc_auth_003(page):
-    base_page = BasePage(page)
-    base_page.open_page(BASE_URL)
-    base_page.validate_url(BASE_URL)
-
-    login_page = LoginPage(page)
-    login_page.enter_username(STANDARD_USER)
-    login_page.validate_username(STANDARD_USER)
-    login_page.enter_password(WRONG_PASSWORD)
-    login_page.validate_password(WRONG_PASSWORD)
-    login_page.click_login_btn()
-    login_page.validate_visibility_of_error()
-    login_page.validate_error_text(NO_MATCHES_ERROR)
-
-def test_tc_auth_004(page):
-    base_page = BasePage(page)
-    base_page.open_page(BASE_URL)
-    base_page.validate_url(BASE_URL)
-
-    login_page = LoginPage(page)
-    login_page.enter_username(FAKE_USER)
-    login_page.validate_username(FAKE_USER)
-    login_page.enter_password(PASSWORD)
-    login_page.validate_password(PASSWORD)
-    login_page.click_login_btn()
-    login_page.validate_visibility_of_error()
-    login_page.validate_error_text(NO_MATCHES_ERROR)
-
-def test_tc_auth_005(page):
-    base_page = BasePage(page)
-    base_page.open_page(BASE_URL)
-    base_page.validate_url(BASE_URL)
-
-    login_page = LoginPage(page)
-    login_page.enter_username(EMPTY_USER)
-    login_page.validate_username(EMPTY_USER)
-    login_page.enter_password(PASSWORD)
-    login_page.validate_password(PASSWORD)
-    login_page.click_login_btn()
-    login_page.validate_visibility_of_error()
-    login_page.validate_error_text(USERNAME_EMPTY_ERROR)
-
-def test_tc_auth_006(page):
-    base_page = BasePage(page)
-    base_page.open_page(BASE_URL)
-    base_page.validate_url(BASE_URL)
-
-    login_page = LoginPage(page)
-    login_page.enter_username(STANDARD_USER)
-    login_page.validate_username(STANDARD_USER)
-    login_page.enter_password(EMPTY_PASSWORD)
-    login_page.validate_password(EMPTY_PASSWORD)
-    login_page.click_login_btn()
-    login_page.validate_visibility_of_error()
-    login_page.validate_error_text(PASSWORD_EMPTY_ERROR)
-
-def test_tc_auth_007(page):
-    base_page = BasePage(page)
-    base_page.open_page(BASE_URL)
-    base_page.validate_url(BASE_URL)
-
-    login_page = LoginPage(page)
-    login_page.enter_username(WRONG_SQL_USER)
-    login_page.validate_username(WRONG_SQL_USER)
-    login_page.enter_password(PASSWORD)
-    login_page.validate_password(PASSWORD)
-    login_page.click_login_btn()
-    login_page.validate_visibility_of_error()
-    login_page.validate_error_text(NO_MATCHES_ERROR)
-
-def test_tc_auth_008(page):
-    base_page = BasePage(page)
-    base_page.open_page(BASE_URL)
-    base_page.validate_url(BASE_URL)
-
-    login_page = LoginPage(page)
-    login_page.enter_username(WRONG_XSS_USER)
-    login_page.validate_username(WRONG_XSS_USER)
-    login_page.enter_password(PASSWORD)
-    login_page.validate_password(PASSWORD)
-    login_page.click_login_btn()
-    login_page.validate_visibility_of_error()
-    login_page.validate_error_text(NO_MATCHES_ERROR)
-
 def test_tc_auth_009(page):
+    base_page = BasePage(page)
+    base_page.open_page(BASE_URL)
+    base_page.validate_url(BASE_URL)
     # пять неуспешных попыток авторизации
+
     for i in range(5):
-        test_tc_auth_008(page)
+        login_page = LoginPage(page)
+        login_page.login_procedure(LOCKED_OUT_USER, PASSWORD)
 
     # шестая попытка успешная (без блокировки страницы)
-    test_tc_auth_001(page)
+    login_page = LoginPage(page)
+    login_page.login_procedure(STANDARD_USER, PASSWORD)
+
+    inventory_page = InventoryPage(page)
+    inventory_page.validate_url(BASE_ROOT + INVENTORY_ENDPOINT)
 
 def test_tc_auth_010(page):
     base_page = BasePage(page)
@@ -135,15 +62,9 @@ def test_tc_auth_010(page):
     base_page.validate_url(BASE_URL)
 
     login_page = LoginPage(page)
-    login_page.enter_username(STANDARD_USER)
-    login_page.validate_username(STANDARD_USER)
-    login_page.enter_password(PASSWORD)
-    login_page.validate_password(PASSWORD)
-    login_page.click_login_btn()
+    login_page.login_procedure(STANDARD_USER, PASSWORD)
 
     inventory_page = InventoryPage(page)
     inventory_page.validate_url(BASE_ROOT + INVENTORY_ENDPOINT)
     inventory_page.reload_page()
     inventory_page.validate_url(BASE_ROOT + INVENTORY_ENDPOINT)
-
-
